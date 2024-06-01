45528d9b5ec4756dcc665e66889e0c0bb4bffca3
Sasha MASNIKOSA <11702482@edu.sorbonne-paris-nord.fr>
2024-06-01 08:24:22 +0200

    Début TP_C exo4 AST

---
+++ AST.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "AST.h"
+
+/* create an AST from a root value and two AST sons */
+AST_expr new_binary_expr(char rule, AST_expr left, AST_expr right) {
+  AST_expr t=(struct _expr_tree*) malloc(sizeof(struct _expr_tree));
+  if (t!=NULL){	/* malloc ok */
+    t->rule=rule;
+    t->left=left;
+    t->right=right;
+  } else printf("ERR : MALLOC ");
+  return t;
+}
+
+/* create an AST from a root value and one AST son */
+AST_expr new_unary_expr(char rule, AST_expr son)
+{
+  return new_binary_expr(rule, NULL,son);
+}
+
+/* create an AST leaf from a value */
+AST_expr new_number_expr(int number)
+{
+  AST_expr t=(struct _expr_tree*) malloc(sizeof(struct _expr_tree));
+  if (t!=NULL){	/* malloc ok */
+    t->rule='N';
+    t->number=number;
+    t->left=NULL;
+    t->right=NULL;
+  } else printf("ERR : MALLOC ");
+  return t;
+}
+
+/* create an AST leaf from a value */
+AST_comm new_command(AST_expr expression){
+  AST_comm t =  malloc(sizeof(struct _command_tree));
+  if (t!=NULL){	/* malloc ok */
+    t->expr1 = expression;
+  } else printf("ERR : MALLOC ");
+  return t;
+
+}
+
+/* delete an AST */
+void free_expr(AST_expr t)
+{
+  if (t!=NULL) {
+    free_expr(t->left);
+    free_expr(t->right);
+    free(t);
+  }
+}
+void free_comm(AST_comm t)
+{
+  if (t!=NULL) {
+    free_expr(t->expr1);
+    free(t);
+  }
+}
+
+/* infix print an AST*/
+void print_expr(AST_expr t){
+  if (t!=NULL) {
+    printf("[ ");
+    print_expr(t->left);
+    if (t->left==NULL) printf(":%d: ",t->number); else printf(":%c: ",t->rule);
+    print_expr(t->right);
+    printf("] ");
+  }
+}
+void print_comm(AST_comm t){
+  if (t!=NULL) {
+    printf("[ ");
+    printf(":%c: ",t->rule);
+    print_expr(t->expr1);
+    printf("] ");
+  }
+
+}
