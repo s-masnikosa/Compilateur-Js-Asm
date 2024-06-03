bfe6308a33401b903e5125da42f80076e07f0a86
Sasha MASNIKOSA <11702482@edu.sorbonne-paris-nord.fr>
2024-06-03 04:43:39 +0200

    frag 2.0

--- AST.c
+++ AST.c
@@ -165,7 +165,7 @@ void print_code_rec(AST_expr t, FILE* output){
 				fprintf(output, "GetVar %s\n", t->var);
 				break;
 			case '=':
-				fprintf(output, "SetVar %s\nGetVar %s\n", t->left->var, t->left->var);
+				fprintf(output, "SetVar %s\n", t->left->var);
 				break;
 			case '+':
 				fprintf(output, "AddiNb\n");
